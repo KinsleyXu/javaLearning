779809af506eb1d644692b3a314b5617f38ed268
xuxinyu06 <xuxinyu06@meituan.com>
2021-04-05 14:11:13 +0800

    删除web

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+Z
+*src/com/hust/javaBase/utils/DateUtils.java,6/b/6b01d02a54b43fa6815fe9b40a50e0aef9921f90
---
+++ src/com/hust/javaBase/utils/DateUtils.java
@@ -0,0 +1,24 @@
+package com.hust.javaBase.utils;
+
+/**
+ * @author xuxinyu06
+ */
+public class DateUtils {
+
+    public static final String DefaultDayFormat = "yyyy-MM-dd";
+    public static final String DefaultSecondFormat = "yyyy-MM-dd HH:mm:ss";
+    public static final String DefaultMinuteFormat = "yyyy-MM-dd HH:mm";
+
+    /**
+     * 获取当晚的时间戳
+     */
+
+
+    /**
+     * 获取当月的时间戳
+     */
+
+    /**
+     * 获取当年的时间戳
+     */
+}
